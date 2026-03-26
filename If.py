ae0e3097e40bb016724dbdebff10e45f5fb59e43
Bulbul <bulbul07biswas@gmail.com>
2026-03-26 15:01:56 +0530

    ifStatement

---
+++ If.py
@@ -0,0 +1,25 @@
+
+#type
+# age= 20
+# if age>21:
+#     print("You can drive")
+# else:
+#     print("You cannot drive")
+
+#type2- Short hand if
+
+# age= 20
+# if age>19: print("You can drive")
+# else: print("You cannot drive")
+
+#type3-elif-It allows us to check multiple conditions
+
+age= 10
+if age<=12:
+    print("You are a child")
+elif age<=19:
+    print("You are an Teenager")
+elif age<=35:
+    print("You are a young adult")
+else:
+    print("You are an adult")
